06deac0f98d456fb2c2d70d935efd157b648a560
Erik Pizana <erikpzcanedo@outlook.com>
2022-01-06 20:19:56 -0600

    Handle transaction action

--- src/Metamask.tsx
+++ src/Metamask.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "./components/connectors";
+import { ethers } from "ethers";
 
 export const Metamask = () => {
-  const x = useWeb3React();
-  console.log(x);
+  const { library, activate } = useWeb3React();
   const [fields, setfields] = useState({ address: "", amount: "" });
+  const [active, setactive] = useState(false);
+  console.log("active", active);
   const handleInputs = (e: any) => {
     setfields({
       ...fields,
@@ -14,50 +16,64 @@ export const Metamask = () => {
     });
   };
   const handleSubmit = async () => {
-    /* console.log(fields);
-    //@ts-ignore
-    const winEther = window.ethereum;
-    try {
-      if (!winEther) {
-        throw new Error("No wallet found");
-      }
-      await winEther.send("eth_requestAccounts");
-      const provider = new ethers.providers.Web3Provider(winEther);
-      const signer = provider.getSigner();
-      ethers.utils.getAddress(fields.address);
-      const tx = await signer.sendTransaction({
-        to: fields.address,
-        value: ethers.utils.parseEther(fields.amount),
-      });
-      console.log("tx", tx);
-    } catch (err: any) {
-      console.log(err.message);
-    } */
+    console.log(fields);
+    const signer = library.getSigner();
+    ethers.utils.getAddress(fields.address);
+    const tx = await signer.sendTransaction({
+      to: fields.address,
+      value: ethers.utils.parseEther(fields.amount),
+    });
+    console.log("tx", tx);
   };
-  /* useEffect(() => {
-    x.activate(injected);
-  }, []); */
+
+  const handleConnect = async () => {
+    await activate(injected);
+    setactive(true);
+  };
+
   return (
     <div>
-      <Box>
-        <Typography my={5}>POC METAMASK</Typography>
-        <TextField
-          name="address"
-          onChange={handleInputs}
-          value={fields.address}
-          label="Address"
-        />
-        <TextField
-          name="amount"
-          type="number"
-          onChange={handleInputs}
-          value={fields.amount}
-          label="Amount"
-        />
-        <Button onClick={handleSubmit} variant="contained">
-          Send ETH
+      <Typography my={5} variant="h2">
+        POC METAMASK
+      </Typography>
+      {!active ? (
+        <Button variant="contained" onClick={handleConnect}>
+          Conectarse a MetaMask
         </Button>
-      </Box>
+      ) : (
+        <Box>
+          <TextField
+            name="address"
+            onChange={handleInputs}
+            value={fields.address}
+            label="Address"
+            fullWidth
+            style={{
+              width: "100%",
+              maxWidth: 400,
+              display: "flex",
+              margin: " 20px auto",
+            }}
+          />
+          <TextField
+            name="amount"
+            type="number"
+            onChange={handleInputs}
+            value={fields.amount}
+            label="Amount"
+            fullWidth
+            style={{
+              width: "100%",
+              maxWidth: 400,
+              display: "flex",
+              margin: "20px auto",
+            }}
+          />
+          <Button onClick={handleSubmit} variant="contained">
+            Send ETH
+          </Button>
+        </Box>
+      )}
     </div>
   );
 };
